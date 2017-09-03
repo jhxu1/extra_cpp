048be8fc0937a71cb3d74fdce00d65a96b7b5ebd
541468099@qq.com <541468099@qq.com>
2017-09-03 09:49:39 +0800

    one

--- greedy.h
+++
@@ -1,7 +0,0 @@
-#ifndef GREEDY_H_INCLUDED
-#define GREEDY_H_INCLUDED
-
-deque<size_t> Greedy(vector<CNode> nodeVec, vector<CLink> linkVec);
-double GetCost(vector<CNode> &nodeVec,vector<CLink> temp_linkVec, deque<size_t> greedyResult);
-
-#endif // GREEDY_H_INCLUDED
